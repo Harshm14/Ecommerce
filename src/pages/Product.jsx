8c9178bdff27d749a97d3a6689a6c3512d0fd140
Harshm14 <harsh.mittal0142@gmail.com>
2024-06-21 12:54:24 +0530

    d

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,6 +3,7 @@ import { ShopContext } from '../context/ShopContext';
 import Breadcrum from '../components/Breadcrum/Breadcrum';
 import ProductDisplay from '../components/ProductDisplay/ProductDisplay';
 import { useParams } from 'react-router-dom';
+import DescriptionBox from '../components/DescriptionBox/DescriptionBox'
 const Product = () => {
     const{all_product}=useContext(ShopContext);
     const{productId}=useParams();
@@ -11,6 +12,7 @@ const Product = () => {
         <div>
             <Breadcrum product={product}></Breadcrum>
             <ProductDisplay product={product}/>
+            <DescriptionBox/>
         </div>
     );
 };
